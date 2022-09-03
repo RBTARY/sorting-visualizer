da9a8f7dcffda6b166d563aa447e6a6a332811f5
RBTARY <92211412+RBTARY@users.noreply.github.com>
2022-09-03 07:27:02 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-  Kunal
     
     *****************
 */
@@ -94,7 +94,7 @@ function runalgo()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Kunal
     
     *****************
 */
