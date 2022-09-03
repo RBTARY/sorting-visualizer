9103a5048d794d9ef6168d342f243bd9c3ad8226
RBTARY <92211412+RBTARY@users.noreply.github.com>
2022-09-03 07:25:56 +0530

    Update bubble_sort.js

--- scripts/bubble_sort.js
+++ scripts/bubble_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Kunal
     
     *****************
 */
@@ -46,7 +46,7 @@ function Bubble()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-  Kunal
     
     *****************
 */
